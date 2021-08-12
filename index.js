42f340dce3d220b6a725e81bbf355ab7b8466cca
prunesland <panargya@gmail.com>
2021-08-12 17:49:55 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import postRoutes from './routes/posts.js'
+
+const app = express()
+// the begining of the url will start after the /posts
+dotenv.config()
+// test commit
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to outstagram app!')
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+mongoose.set('useFindAndModify', false);
